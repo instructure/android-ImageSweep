ec3db06493bf6839166b595bdd310cfff9af6a25
Josh Ruesch <joshr@instructure.com>
2014-04-18 16:01:53 -0600

    Initial code commit.

---
+++ ImageSweep.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+"""ImageSweep.py: Deletes unnecessary image drawables.."""
+
+__author__      = "Josh Ruesch"
+__copyright__   = "Copyright 2014, Instructure Inc"
+
+import os
+import re
+import sys
+
+#Global variables.
+used_drawable_files = set()
+files_deleted = 0
+mega_bytes_deleted = 0
+
+#Naive method to determine if a directory is an android /res folder.
+def isResourceRoot(directory):
+  return (
+  (os.path.exists(directory+"/drawable"))        or
+  (os.path.exists(directory+"/drawable-ldpi"))   or
+  (os.path.exists(directory+"/drawable-mdpi"))   or
+  (os.path.exists(directory+"/drawable-hdpi"))   or
+  (os.path.exists(directory+"/drawable-xhdpi"))  or
+  (os.path.exists(directory+"/drawable-xxhdpi")) or
+  (os.path.exists(directory+"/drawable-xxxhdpi")))
+
+#We only want to remove unused PICTURES (pngs)
+def addFile(fileName):
+  fileName = fileName.replace("R.drawable.", "").replace("@drawable/","")
+  used_drawable_files.add(fileName + ".png")
+
+#Check to see what resources are referenced in this function.
+def checkFileForResources(fileAsString):
+  file = open(fileAsString, 'r')
+  contents = file.read()
+  file.close()
+
+  #Handle code files.
+  pattern = re.compile('R.drawable.[a-zA-Z0-9_]*')
+  results = pattern.findall(contents)
+  for result in results:
+    addFile(result)
+
+  #Handle layout files.
+  pattern = re.compile('@drawable/[a-zA-Z0-9_]*')
+  results = pattern.findall(contents)
+  for result in results:
+    addFile(result)
+
+#We only want to if it's an unreferenced PNG.
+def deleteIfUnusedPNG(directory, fileName):
+    if fileName.endswith(".png") and not fileName.endswith(".9.png"):
+      if fileName not in used_drawable_files:
+        global files_deleted
+        global mega_bytes_deleted
+
+        #Do stats tracking.
+        files_deleted += 1
+        current_file_size = os.path.getsize(directory+"/"+fileName) / 1024.0 / 1024.0
+        mega_bytes_deleted += current_file_size
+
+        #Actually delete the file.
+        os.unlink(directory+"/"+fileName)
+        print ("Deleted (%.2f Mbs): " + directory+"/"+fileName) % current_file_size
+
+##########
+## MAIN ##
+##########
+
+#Make sure they passed in a project source directory.
+if not len(sys.argv) == 2:
+  print 'Usage: "python ImageSweep.py project_src_directory"'
+  quit()
+
+rootDirectory = sys.argv[1]
+resDirectory = rootDirectory
+
+#Figure out which resources are actually used.
+for root, dirs, files in os.walk(rootDirectory):
+  if isResourceRoot(root):
+    resDirectory = root
+
+  for file in files:
+    checkFileForResources(root+"/"+file)
+
+#Delete the unused pngs.
+for root, dirs, files in os.walk(resDirectory):
+    for file in files:
+      deleteIfUnusedPNG(root, file)
+
+#Print out how many files were actually deleted.
+print ""
+print "%d file(s) deleted" % (files_deleted)
+print "%.2f megabytes freed" % (mega_bytes_deleted)
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-android-ImageSweep
-==================
+## ImageSweep
 
-Python script used to remove all unused image resources.
+ImageSweep is a python script used to remove unused drawables from your application. During the build process, all indexed .pngs inside of 'drawable' folders get crunched so that their size is much much smaller; However, even unused images get bundled with your apk. This means that your apk is potentially much much larger than it needs to be even after compression.
+
+##Usage
+		 python image_sweep.py project_src_directory
+
+Where project_src_directory is the relative or absolute file-path where your source code lives. Make sure the chosen directory contains ALL of your source code / AndroidManifest.xml, but none of the libraries you've included (the script auto-determines where the /res folder is and libraries can potentially break that).
+
+##How It Works
+At a high level, the script scans each file and does a search for 'R.drawable.' or "@drawable/". Afterwards it checks each resource inside of drawable folders to see if it was referenced anywhere.
+
+
+## How Well Does it Work
+In the 'Canvas for Android' project, we were able to cut about 46% off the raw apk size and 26% off the installed app size. It works best as a supplement to ProGuard.
+
+##Warnings!
+* This script only works for top-level projects that aren't referenced from other projects.
+* The files deleted **cannot** be reversed. **Please only run this script on projects located in source control.** Just in case.
